25e7b68ca89b70a685de8dadb6f320fc61dfd5e6
mone19 <fuocoangelos@yahoo.de>
2024-01-10 14:30:48 +0100

    neues Layout

--- frontend/home/home.js
+++ frontend/home/home.js
@@ -1,13 +1,22 @@
-function logout() {
-    document.getElementById('logoutButton').addEventListener('click', function () {
-        fetch('/logout', {
-            method: 'GET',
-        })
-            .then(response => {
-                if (response.redirected) {
-                    window.location.href = response.url;
-                }
-            })
-            .catch(error => console.error('Error:', error));
+
+const logoutButton = document.getElementById('logoutButton');
+
+logoutButton.addEventListener('click', async () => {
+  try {
+    const response = await fetch("http://localhost:3000/api/logout", {
+      method: 'GET',
     });
-}
+
+    if (response.ok) {
+      window.location.href = '/'
+    } else {
+      console.error('Logout fehlgeschlagen.');
+    
+    }
+  } catch (error) {
+    console.error('Fehler beim Logout:', error);
+    
+  }
+});
+
+
